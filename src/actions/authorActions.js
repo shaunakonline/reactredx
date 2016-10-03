0cc742699d58236761470246ee2db9ab73cc4340
shaunakonline <shaunaky@gmail.com>
2016-10-02 22:16:24 -0700

    completed error handling module

---
+++ src/actions/authorActions.js
@@ -0,0 +1,22 @@
+import * as types from './actionTypes';
+import authorApi from '../api/mockAuthorApi';
+import {beginAjaxCall} from './ajaxStatusActions';
+
+// export function createCourse(course) {
+//   return{ type: types.CREATE_COURSE, course};
+// }
+export function loadAuthorsSuccess(authors) {
+  return{ type: types.LOAD_AUTHORS_SUCCESS, authors};
+}
+
+export function loadAuthors(){
+return function(dispatch){
+    dispatch(beginAjaxCall());
+  return authorApi.getAllAuthors().then( (authors) =>{
+    dispatch(loadAuthorsSuccess(authors));
+  }).catch( error =>{
+    // /dispatch(ajaxCallError(error));
+    throw(error);
+  });
+};
+}
--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,40 @@
-export function createCourse(course) {
-  debugger;
-  return{ type: 'CREATE_COURSE', course};
+import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
+export function loadCoursesSuccess(courses) {
+  return{ type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+export function createCourseSuccess(course) {
+  return {type: types.CREATE_COURSE_SUCCESS, course};
+}
+
+export function updateCourseSuccess(course) {
+  return {type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
+export function loadCourses(){
+return function(dispatch){
+  dispatch(beginAjaxCall());
+  return courseApi.getAllCourses().then( (courses) =>{
+    dispatch(loadCoursesSuccess(courses));
+  }).catch( error =>{
+    dispatch(ajaxCallError(error));
+    throw(error);
+  });
+};
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    dispatch(beginAjaxCall());
+    return courseApi.saveCourse(course).then(course => {
+      course.id ? dispatch(updateCourseSuccess(course)) :
+        dispatch(createCourseSuccess(course));
+    }).catch(error => {
+      dispatch(ajaxCallError(error));
+      throw(error);
+    });
+  };
 }
